a3a42b55513d976ea6ddf88e5d441153d3d0adf3
Mehdi.cj <mahdimousavi1385.7@gmail.com>
2025-08-04 23:05:52 +0330

    Add files via upload

---
+++ mind reading machine.py
@@ -0,0 +1,66 @@
+first_number = input("choose a number between 1 - 20 :")
+
+first_number = int(first_number)
+number = first_number
+
+print('....................')
+
+input('Multiply the selected number by 2')
+# number = number * (2)
+number *= (2)
+
+print('....................')
+
+
+input('Add the answer to the number 8')
+# number = number + (8)
+number += (8)
+
+print('....................')
+
+
+input('Add the answer to the first number you chose')
+# number = number + first_number
+number += first_number
+
+print('....................')
+
+
+input('Subtract 2 from your answer')
+# number = number - (2)
+number -= (2)
+
+print('....................')
+
+
+input('Divide the answer by 3')
+# number = number / (3)
+number /= (3)
+
+print('....................')
+
+
+input('Subtract the first number you chose from the answer')
+# number = number - first_number
+number -= first_number
+
+print('....................')
+
+
+input('Multiply the answer by 4')
+# number = number * (4)
+number *= (4)
+
+print('....................')
+
+
+bmbc = (' the 8 number is your answer')
+
+print(number)
+print(bmbc)
+print('            ')
+answer = input("Did we really read your mind ? (yes/No) ")
+print("            ")
+input('Thank you for your support')
+print('            ')
+input('goodbye')
