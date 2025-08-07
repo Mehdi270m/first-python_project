459db483d9f8611852b2673dad975cc7a485bb4f
Mehdi.cj <mahdimousavi1385.7@gmail.com>
2025-08-07 20:17:43 +0330

    Update login.py

--- login.py
+++ login.py
@@ -10,9 +10,6 @@ inter_username = input("Please enter your username: ")
 
 inter_password = input('Please enter your password:')
 
-# if inter_username in users and users[inter_username] == inter_password :
-#     print('welcome to my saits ')
-
 while inter_username not in users or inter_password != users[inter_username] :
     print('')
     print('Username does not match the password')
@@ -24,3 +21,4 @@ print('You successfully arrived.' \
 
 
  
+
